2750de4f372cc08daf79aedadbee4d63e8b3c051
acartagenes <70467344+acartagenes@users.noreply.github.com>
2020-10-22 03:52:32 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,8 +1,12 @@
 const CANVAS_WIDTH = 600;
 const CANVAS_HEIGHT = 600;
-const BG_COLOR = [128, 30, 200];
+let bg;
 let ship;
 let shipAnim;
+let fly;
+let ufo; 
+let tentacle;
+
 
 function preload() {
   const shipSpritesheet = loadSpriteSheet("img/SpaceShip.png", 32, 32, 12);
@@ -10,6 +14,20 @@ function preload() {
   ship = createSprite(CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2, 32, 32);
   ship.moveSpeed = 5;
   ship.scale = (2.0)
+
+  const AlienSpritesheet = loadSpriteSheet("img/Aliens Fly.png", 64, 64, 2);
+  fly = loadAnimation(AlienSpritesheet)
+  
+
+  const UfoSpritesheet = loadSpriteSheet("img/Aliens clone.png", 64, 64, 5);
+  ufo = loadAnimation(UfoSpritesheet)
+
+  const tenSpritesheet = loadSpriteSheet("img/Tentacle.png", 128, 128, 5);
+  tentacle = loadAnimation(tenSpritesheet)
+
+  bg = loadImage('img/space.jpg')
+  
+
 }
 
 function setup() {
@@ -17,6 +35,7 @@ function setup() {
   ship.addAnimation("move", shipAnim);
   ship.addImage("still", loadImage("img/ship_still.png"));
   ship.setDefaultCollider();
+  
 }
 
 function update(object) {
@@ -52,6 +71,10 @@ function drawObject(object) {
 }
 
 function draw() {
-  background(BG_COLOR);
+  background(bg);
   update(ship);
+  animation(fly, 300, 100);
+  animation(ufo, 400,250);
+  animation(tentacle, 150,300)
+
 }
